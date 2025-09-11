5d1ca6734baa10a87df18aa6e6a61ecc10ca1b58
Amith B V <amithrao0606@gmail.com>
2025-09-11 13:31:12 +0530

    Updated mailer.ts file to send the email for user verification and forgot password.

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,15 +1,30 @@
+import User from "@/models/userModels";
+import bcrypt from "bcryptjs";
 import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
     // TODO : Configure mail for usage
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
+    const hashedToken = await bcrypt.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    const transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: "amithrao0606@gmail.com",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: "129c9b31d73fe8",
+        pass: "****0eb8",
       },
     });
 
@@ -18,13 +33,25 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       to: email,
       subject:
         emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      html: "<b>Hello world?</b>",
+      html:
+        emailType === "VERIFY"
+          ? verifyEmailTemplate(hashedToken)
+          : forgotPasswordTemplate(hashedToken),
     };
 
-    const mailResponse = await transporter.sendMail(mailOptions);
+    const mailResponse = await transport.sendMail(mailOptions);
 
     return mailResponse;
   } catch (error: any) {
     throw new Error(error.message);
   }
+};
+
+const verifyEmailTemplate = (hashedToken: string) => {
+  return `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to verify your email or copy and paste the link below in your browser.<br /> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`;
+};
+
+const forgotPasswordTemplate = (hashedToken: string) => {
+  return `<p>Click <a href="${process.env.DOMAIN}/forgotpassword?token=${hashedToken}">here</a> to reset your password or copy and paste the link below in your browser.<br />${process.env.DOMAIN}/forgotpassword?token=${hashedToken}
+</p>`;
 };
