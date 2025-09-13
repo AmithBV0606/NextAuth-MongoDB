365883e9514555b81005789f537c9e7e28336729
Amith B V <amithrao0606@gmail.com>
2025-09-13 09:40:18 +0530

    Added api route for logout.

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,27 @@
+import { ConnectDB } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+ConnectDB();
+
+export async function GET(request: NextRequest) {
+  try {
+    const response = NextResponse.json({
+      message: "Logout successfull.",
+      success: true,
+    });
+
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
